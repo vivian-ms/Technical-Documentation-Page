bb9caf690f74954154e91d97f5e4305fdcd4916a
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2019-10-22 23:39:42 -0700

    Changed code to determine whether arrow is needed (yes for top fixed navbar and no for sidebar)

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 $(function() {
+  // On page load, determine if arrow on navbar header is needed
+  resizeMenu();
+
+  // Event listener for window resize
+  $(window).on('resize', resizeMenu);
+
   // Event listener for navbar header click
-  $('#navbar header').on('click', function(evt) {
+  $('#navbar header').on('click', toggleMenu);
+});
+
+
+function resizeMenu() {
+  let w = window.innerWidth;
+
+  // If screen width < 700px, display arrow
+  if (w < 700) {
+    if ($('#navbar li').css('display') === 'none') {
+      // If nav list is hidden, display arrow up
+      $('#arrow').html('&#xfe3f;');
+    } else {
+      // If nav list is shown, display arrow down
+      $('#arrow').html('&#xfe40;');
+    }
+  } else {
+    // If screen width > 700px, don't display arrow
+    $('#arrow').text('');
+  }
+}
+
+
+function toggleMenu() {
+  let w = window.innerWidth;
+
+  if (w < 700) {
+    // If screen width < 700px, toggle #navbar li display (none <-> inline-block) on #navbar header click
     $('#navbar li').toggleClass('showMenu');
 
     if ($('#navbar li').css('display') === 'none') {
@@ -10,5 +43,5 @@ $(function() {
       // If nav list is shown, display arrow down
       $('#arrow').html('&#xfe40;');
     }
-  });
-});
+  }
+}
